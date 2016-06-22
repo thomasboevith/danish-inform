78a9cb865b421729b4f67f0392d5bfb4df7e60d5
Thomas Bøvith <thomasboevith@users.noreply.github.com>
2016-06-22 08:28:27 -0700

    Initial commit

---
+++ danish_alphabet.h
@@ -0,0 +1,7 @@
+! Z-machine alphabet table for Danish language.
+
+#ifdef TARGET_ZCODE;
+Zcharacter "abcdefghijklmnopårstuvæxyø"
+           "ABCDEFGHIJKLMNOPÅRSTUVÆXYØ"
+              "012345.,!?'/-:()wqzWQZé";
+#endif;
