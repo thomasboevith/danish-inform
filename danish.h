8602d7ea044fe7efde82e69bb8deb2d8ee7249fe
Thomas Bøvith <thomasboevith@users.noreply.github.com>
2016-07-08 09:36:08 -0700

    Change encoding to unicode

--- danish.h
+++ danish.h
@@ -1,7 +1,8 @@
+! -*- coding: utf-8 -*-
 ! ==============================================================================
 !   DANISH:  Language Definition File
 !
-!   Derived from English.h dated 20160608 by Thomas Bøvith (2016)
+!   Derived from English.h dated 20160608 by Thomas BÃ¸vith (2016)
 !
 !   Supplied for use with Inform 6 -- Release 6.12.1 -- Serial number 160605
 !
