85a7d57be0df4d553f1ab087b9e00229e4f38ea5
Thomas Bøvith <thomasboevith@users.noreply.github.com>
2016-07-09 00:13:17 -0700

    Translate numbers

--- danish.h
+++ danish.h
@@ -165,10 +165,10 @@ Array LanguageDescriptors table
     'unlit'   $$111111111111    (-light)        NULL;
 
 Array LanguageNumbers table
-    'one' 1 'two' 2 'three' 3 'four' 4 'five' 5
-    'six' 6 'seven' 7 'eight' 8 'nine' 9 'ten' 10
-    'eleven' 11 'twelve' 12 'thirteen' 13 'fourteen' 14 'fifteen' 15
-    'sixteen' 16 'seventeen' 17 'eighteen' 18 'nineteen' 19 'twenty' 20;
+    'en' 1 'et' 1 'to' 2 'par' 2 'tre' 3 'fire' 4 'fem' 5
+    'seks' 6 'syv' 7 'otte' 8 'ni' 9 'ti' 10
+    'elleve' 11 'tolv' 12 'tretten' 13 'fjorten' 14 'femten' 15
+    'seksten' 16 'sytten' 17 'atten' 18 'nitten' 19 'tyve' 20;
 
 ! ------------------------------------------------------------------------------
 !   Part III.   Translation
