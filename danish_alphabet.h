a3c383407afaf33e14fb5d5b4adc2c7bb19252fc
Thomas Bøvith <thomasboevith@users.noreply.github.com>
2017-05-12 09:04:29 -0700

    Deleting files that were moved to v6.12 directory

--- danish_alphabet.h
+++
@@ -1,7 +0,0 @@
-! Z-machine alphabet table for Danish language.
-
-#ifdef TARGET_ZCODE;
-Zcharacter "abcdefghijklmnopårstuvæxyø"
-           "ABCDEFGHIJKLMNOPÅRSTUVÆXYØ"
-              "012345.,!?'/-:()wqzWQZé";
-#endif;
